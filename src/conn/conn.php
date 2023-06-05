a931365665a0d65f815aaf09b7e5624f1d6b87bf
Pablo <pablo.sm14@aluno.ifsc.edu.br>
2023-06-05 01:27:28 -0300

    Modificando classe do banco para testar em produção

--- src/conn/conn.php
+++ src/conn/conn.php
@@ -5,19 +5,27 @@ class Database
     protected static $db;
     private function __construct()
     {
-        $db_host = 'localhost';
-        $db_user = 'root';
-        $db_password = '';
-        $db_name = 'bdteste';
-        $db_driver = "mysql";
-        $options = array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION );
-      
+        // $db_host = 'localhost';
+        // $db_user = 'root';
+        // $db_password = '';
+        // $db_name = 'bdteste';
+        // $db_driver = "mysql";
+        // $options = array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION );
+        $host     = getenv("host");
+        $username = "root";
+        $pass     = getenv("pass");
+        $port     = getenv("port");
+        $db       = "railway";
+
+        $connecthost = "mysql:host=$host;dbname=$db;port=$port";
+        
         try
         {
-            self::$db = new PDO("$db_driver:host=$db_host; dbname=$db_name", $db_user, $db_password, $options);
+            self::$db = new \PDO($connecthost, $username, $pass);
             self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // self::$db->exec('SET NAMES utf8');
             self::$db->exec('SET NAMES utf8mb4');
+            self::$db->exec("SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));");
         }
         catch (PDOException $e)
         {   
